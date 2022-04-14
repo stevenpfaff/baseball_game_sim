6adeb07582f55db92e3ebf971545c121721ed727
stevenpfaff <pfaffy1426@gmail.com>
2022-04-14 16:44:32 -0500

    added entire at bat function

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
-function randomResult() {
-    let randomHit = Math.floor(Math.random() * 101);
-    if (randomHit < 40) {
-        console.log('Hit!')
-    }
-    else if (randomHit > 40) {
-        console.log('Out')
-    }
-}
-randomResult()
+function atBat() {
+    let result = Math.floor(Math.random() * 101)
+    if (result < 20)
+        console.log('Walk')
 
+    else if (result < 40 && result > 20)
+        console.log('Strikeout')
+
+    else if (result < 60 && result > 40)
+        console.log('Out')
+
+    else if (result < 100 && result > 60)
+        console.log('Hit!')
+
+}
+atBat()
 
 
