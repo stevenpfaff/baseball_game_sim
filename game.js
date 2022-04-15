8578c81174c522d4e0f1f42ddad7ff9dac793460
stevenpfaff <pfaffy1426@gmail.com>
2022-04-15 10:15:01 -0500

    first player entered in with looping code

--- game.js
+++ game.js
@@ -1,18 +1,47 @@
-function atBat() {
-    let result = Math.floor(Math.random() * 101)
-    if (result < 20)
-        console.log('Walk')
+function hudson() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 40)
+            console.log('Walk')
 
-    else if (result < 40 && result > 20)
-        console.log('Strikeout')
+        else if (result < 70 && result > 40)
+            console.log('Strikeout')
 
-    else if (result < 60 && result > 40)
-        console.log('Out')
+        else if (result < 80 && result > 70)
+            console.log('Out')
 
-    else if (result < 100 && result > 60)
-        console.log('Hit!')
+        else if (result < 90 && result > 80)
+            console.log('Single!')
 
+        else if (result < 95 && result > 90)
+            console.log('Double!!')
+
+        else if (result < 99 && result > 95)
+            console.log('Triple!!!')
+
+        else if (result < 100 && result > 99)
+            console.log('Homerun!!!!!!')
+    }
 }
-atBat()
+hudson()
 
+// function pitcher() {
+//     let result = Math.floor(Math.random() * 101)
+//     if (result < 40)
+//         console.log('Walk')
 
+//     else if (result < 70 && result > 40)
+//         console.log('Strikeout')
+
+//     else if (result < 100 && result > 90)
+//         console.log('Out')
+
+//     else if (result < 90 && result > 70)
+//         console.log('Hit!')
+
+// }
+// //pitcher()
