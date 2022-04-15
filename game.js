eaaa8d720d2cd1d0eeadd7cf443e129e2489ad3c
stevenpfaff <pfaffy1426@gmail.com>
2022-04-15 12:45:18 -0500

    all players entered

--- game.js
+++ game.js
@@ -27,21 +27,297 @@ function hudson() {
             console.log('Homerun!!!!!!')
     }
 }
-hudson()
 
-// function pitcher() {
-//     let result = Math.floor(Math.random() * 101)
-//     if (result < 40)
-//         console.log('Walk')
+function samuel() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 25)
+            console.log('Walk')
 
-//     else if (result < 70 && result > 40)
-//         console.log('Strikeout')
+        else if (result < 40 && result > 25)
+            console.log('Strikeout')
 
-//     else if (result < 100 && result > 90)
-//         console.log('Out')
+        else if (result < 65 && result > 40)
+            console.log('Out')
 
-//     else if (result < 90 && result > 70)
-//         console.log('Hit!')
+        else if (result < 90 && result > 65)
+            console.log('Single!')
 
-// }
-// //pitcher()
+        else if (result < 95 && result > 90)
+            console.log('Double!!')
+
+        else if (result < 99 && result > 95)
+            console.log('Triple!!!')
+
+        else if (result < 100 && result > 99)
+            console.log('Homerun!!!!!!')
+    }
+}
+
+function cooper() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 25)
+            console.log('Walk')
+
+        else if (result < 40 && result > 25)
+            console.log('Strikeout')
+
+        else if (result < 65 && result > 40)
+            console.log('Out')
+
+        else if (result < 85 && result > 65)
+            console.log('Single!')
+
+        else if (result < 90 && result > 80)
+            console.log('Double!!')
+
+        else if (result < 97 && result > 90)
+            console.log('Triple!!!')
+
+        else if (result < 100 && result > 97)
+            console.log('Homerun!!!!!!')
+    }
+}
+
+function gavin() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 25)
+            console.log('Walk')
+
+        else if (result < 45 && result > 25)
+            console.log('Strikeout')
+
+        else if (result < 65 && result > 45)
+            console.log('Out')
+
+        else if (result < 85 && result > 65)
+            console.log('Single!')
+
+        else if (result < 90 && result > 80)
+            console.log('Double!!')
+
+        else if (result < 97 && result > 90)
+            console.log('Triple!!!')
+
+        else if (result < 100 && result > 97)
+            console.log('Homerun!!!!!!')
+    }
+}
+function jace() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 15)
+            console.log('Walk')
+
+        else if (result < 40 && result > 15)
+            console.log('Strikeout')
+
+        else if (result < 70 && result > 40)
+            console.log('Out')
+
+        else if (result < 90 && result > 70)
+            console.log('Single!')
+
+        else if (result < 95 && result > 90)
+            console.log('Double!!')
+
+        else if (result < 99 && result > 95)
+            console.log('Triple!!!')
+
+        else if (result < 100 && result > 99)
+            console.log('Homerun!!!!!!')
+    }
+}
+function chase() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 20)
+            console.log('Walk')
+
+        else if (result < 40 && result > 20)
+            console.log('Strikeout')
+
+        else if (result < 65 && result > 40)
+            console.log('Out')
+
+        else if (result < 90 && result > 65)
+            console.log('Single!')
+
+        else if (result < 95 && result > 90)
+            console.log('Double!!')
+
+        else if (result < 99 && result > 95)
+            console.log('Triple!!!')
+
+        else if (result < 100 && result > 99)
+            console.log('Homerun!!!!!!')
+    }
+}
+function mike() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 15)
+            console.log('Walk')
+
+        else if (result < 45 && result > 15)
+            console.log('Strikeout')
+
+        else if (result < 70 && result > 45)
+            console.log('Out')
+
+        else if (result < 90 && result > 70)
+            console.log('Single!')
+
+        else if (result < 95 && result > 90)
+            console.log('Double!!')
+
+        else if (result < 99 && result > 95)
+            console.log('Triple!!!')
+
+        else if (result < 100 && result > 99)
+            console.log('Homerun!!!!!!')
+    }
+}
+function brantley() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 25)
+            console.log('Walk')
+
+        else if (result < 50 && result > 25)
+            console.log('Strikeout')
+
+        else if (result < 65 && result > 50)
+            console.log('Out')
+
+        else if (result < 90 && result > 65)
+            console.log('Single!')
+
+        else if (result < 95 && result > 90)
+            console.log('Double!!')
+
+        else if (result < 99 && result > 95)
+            console.log('Triple!!!')
+
+        else if (result < 100 && result > 99)
+            console.log('Homerun!!!!!!')
+    }
+}
+function jaxson() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 25)
+            console.log('Walk')
+
+        else if (result < 65 && result > 25)
+            console.log('Strikeout')
+
+        else if (result < 80 && result > 65)
+            console.log('Out')
+
+        else if (result < 90 && result > 80)
+            console.log('Single!')
+
+        else if (result < 98 && result > 90)
+            console.log('Double!!')
+
+        else if (result < 99 && result > 98)
+            console.log('Triple!!!')
+
+        else if (result < 100 && result > 99)
+            console.log('Homerun!!!!!!')
+    }
+}
+function hannah() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 20)
+            console.log('Walk')
+
+        else if (result < 55 && result > 20)
+            console.log('Strikeout')
+
+        else if (result < 80 && result > 55)
+            console.log('Out')
+
+        else if (result < 97 && result > 80)
+            console.log('Single!')
+
+        else if (result < 100 && result > 98)
+            console.log('Double!!')
+
+    }
+}
+function brinton() {
+    for (var i = 0; i < 75; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 25)
+            console.log('Walk')
+
+        else if (result < 55 && result > 25)
+            console.log('Strikeout')
+
+        else if (result < 80 && result > 55)
+            console.log('Out')
+
+        else if (result < 97 && result > 80)
+            console.log('Single!')
+
+        else if (result < 100 && result > 98)
+            console.log('Double!!')
+
+    }
+}
+//jace()
+//samuel()
+//gavin()
+//cooper()
+//chase()
+//mike()
+//brantley()
+//jaxson()
+//hannah()
+brinton()
+//hudson()
