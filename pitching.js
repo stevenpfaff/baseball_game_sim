d06e29e22bc3e9e9d32b7510624c58b4ab360fa5
stevenpfaff <pfaffy1426@gmail.com>
2022-04-18 11:27:27 -0500

    added button to HTML page to randomize

--- pitching.js
+++ pitching.js
@@ -1,5 +1,5 @@
 function jace() {
-    for (var i = 0; i < 15; i++) {
+    for (var i = 0; i < 20; i++) {
         var result = 0;
         var loopEnd = Math.floor(Math.random() * 100 + 1);
         for (var j = 0; j < loopEnd; j++) {
@@ -15,7 +15,41 @@ function jace() {
             console.log('Hit')
     }
 }
-jace()
+function samuel() {
+    for (var i = 0; i < 20; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 10)
+            console.log('Walk')
+        else if (result < 35 && result > 10)
+            console.log('Strikeout')
+        else if (result < 60 && result > 35)
+            console.log('Out')
+        else if (result < 100 && result > 60)
+            console.log('Hit')
+    }
+}
+function gavin() {
+    for (var i = 0; i < 20; i++) {
+        var result = 0;
+        var loopEnd = Math.floor(Math.random() * 100 + 1);
+        for (var j = 0; j < loopEnd; j++) {
+            result++;
+        }
+        if (result < 15)
+            console.log('Walk')
+        else if (result < 40 && result > 15)
+            console.log('Strikeout')
+        else if (result < 60 && result > 40)
+            console.log('Out')
+        else if (result < 100 && result > 60)
+            console.log('Hit')
+    }
+}
+//jace()
 //samuel()
 //gavin()
 //cooper()
